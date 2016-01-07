5195756a5e5a47974f6015e5502a25f89fa110cc
jackkkk21 <id.mecall@yahoo.com>
2016-01-06 22:13:28 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {176894771,176894771,0,tonumber(our_id)},--Sudo users
+    sudo_users = {176894771},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
